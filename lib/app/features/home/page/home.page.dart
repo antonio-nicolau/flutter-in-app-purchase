c4da4f759abce2944ed8e5b594c574b14cb2eeaa
antonionicolau <antonio@keleya.de>
2022-07-23 00:05:28 +0100

    Designed home page with mock data

--- lib/app/features/home/page/home.page.dart
+++ lib/app/features/home/page/home.page.dart
@@ -1,8 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:in_app_purchase_flutter/app/features/home/widgets/product_card.widget.dart';
+import 'package:in_app_purchase_flutter/app/models/product.model.dart';
+import 'package:in_app_purchase_flutter/app/services/in_app_purchase.service.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
+  final InAppPurchaseService inAppPurchaseService = InAppPurchaseService();
+
+  final products = [
+    Product(
+      title: 'Test subscription',
+      price: 34.9,
+      productId: 'in.app.purchase.subscription',
+      productType: ProductType.subscription,
+    ),
+    Product(
+      title: 'Test product',
+      price: 199.99,
+      productId: 'in.app.purchase.product',
+      productType: ProductType.product,
+    ),
+  ];
+
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    inAppPurchaseService
+        .getProductsDetails(<String>{'in.app.purchase.testeproduct'}).then((value) => print('Result: ${value?.first.title}'));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Products',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+
+                  return ProductCard(product);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/app/features/home/widgets/product_card.widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:in_app_purchase_flutter/app/models/product.model.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard(this.product, {Key? key}) : super(key: key);
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Card(
+        elevation: 2,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    product.title,
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text(
+                      product.productType.name,
+                      style: Theme.of(context).textTheme.subtitle1?.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Buy \$ ${product.price}', style: Theme.of(context).textTheme.subtitle2),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
